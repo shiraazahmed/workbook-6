6d8b3be67d5588b6f21409d856d1e4b07c37d4dc
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-05-21 13:45:44 -0400

    completed

---
+++ WorkingWithInterfaces/src/main/java/com/pluralsight/Comparable.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public interface Comparable<P> {
+    int compareTo(P o);
+
+
+
+
+
+
+}
---
+++ WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.lastName.compareTo(o.lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
