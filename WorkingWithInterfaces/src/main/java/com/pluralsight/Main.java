8c28077476a566bb3b1bed3ac341bf78dfe94867
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-05-21 15:05:13 -0400

    completed Streams

--- WorkingWithInterfaces/src/main/java/com/pluralsight/Main.java
+++ WorkingWithInterfaces/src/main/java/com/pluralsight/Main.java
@@ -12,6 +12,32 @@ public class Main {
         myFamily.add(new Person("Ian", "Auston", 16));
         myFamily.add(new Person("Zephaniah", "Hughes", 9));
         myFamily.add(new Person("Ezra", "Aiden", 17));
+        myFamily.add(new Person("Zachary", "Wyatt", 31));
+        myFamily.add(new Person("Zach", "Lukas", 31));
+        myFamily.add(new Person("Tom", "Domyn", 35));
+        myFamily.add(new Person("Ranveer", "Singh", 45));
+
+
+        myFamily.add(new Person("Jalen", "Williams", 23));
+        myFamily.add(new Person("Grant", "Williams", 25));
+        myFamily.add(new Person("Jaden", "Ivey", 22));
+        myFamily.add(new Person("Keyontae", "Johnson", 24));
+        myFamily.add(new Person("Keldon", "Johnson", 24));
+        myFamily.add(new Person("Draymond", "Green", 34));
+        myFamily.add(new Person("Josh", "Green", 23));
+        myFamily.add(new Person("Trey", "Murphy", 24));
+
+        String lastName = "Wyatt";
+        List<Person> matchingPeople = new ArrayList<>();
+        for (Person person : myFamily) {
+            if (person.getLastName().equals(lastName)) {
+                matchingPeople.add(person);
+            }
+        }
+        for (Person person : matchingPeople) {
+            System.out.println(person);
+        }
+
 
         System.out.println(myFamily);
 
